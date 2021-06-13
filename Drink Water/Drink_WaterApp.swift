facae1b752b22fe8e2a030cf659de13f3bf1b490
Giorgio Giannotta <jorge_upa@msn.com>
2021-06-13 14:19:34 +0200

    Initial Commit

---
+++ Drink Water/Drink_WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drink_WaterApp.swift
+//  Drink Water
+//
+//  Created by Jorge Giannotta on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Drink_WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
