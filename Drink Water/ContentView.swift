ba5636ecaa26813c43cc0f633647ae9f9e3f3bee
Giorgio Giannotta <jorge_upa@msn.com>
2021-06-13 15:06:19 +0200

    Done UI

--- Drink Water/ContentView.swift
+++ Drink Water/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    var screenWidth = UIScreen.main.bounds.width
+    @State var dynamicHeight = 0
+    @State var text = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+            ZStack{
+                Rectangle()
+                    .padding(.bottom,0)
+                    .frame(width: screenWidth, height: CGFloat(dynamicHeight), alignment: .center)
+                    .foregroundColor(.blue)
+                    .overlay(
+                        HStack(alignment: .center, spacing: nil, content: {
+                            Button(action: {
+                                if dynamicHeight > 0 {
+                                    dynamicHeight -= 100
+                                    text -= 250
+                                }
+                            }, label: {
+                                Circle()
+                                    .frame(width: 100, height: 100, alignment: .center)
+                                    .foregroundColor(.red)
+                                    .overlay(Text("Remove").foregroundColor(.white))
+                            })
+                            .padding()
+                            Spacer()
+                            Text("\(text)")
+                                .foregroundColor(.white)
+                            Spacer()
+                            Button(action: {
+                                if dynamicHeight <= 900{
+                                    dynamicHeight += 100
+                                    text += 250
+                                }
+                            }, label: {
+                                Circle()
+                                    .frame(width: 100, height: 100, alignment: .center)
+                                    .foregroundColor(.green)
+                                    .overlay(Text("Add").foregroundColor(.white))
+                            })
+                            .padding()
+                        })
+                        ,alignment: .center
+                    )
+            }
+        })
+        
     }
 }
 
---
+++ Drink Water/WaveForm.swift
@@ -0,0 +1,20 @@
+//
+//  WaveForm.swift
+//  Drink Water
+//
+//  Created by Jorge Giannotta on 13/06/21.
+//
+
+import SwiftUI
+
+struct WaveForm: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct WaveForm_Previews: PreviewProvider {
+    static var previews: some View {
+        WaveForm()
+    }
+}
