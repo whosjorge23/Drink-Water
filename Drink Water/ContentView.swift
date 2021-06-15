b5137892e09678931c352c18a9a4a6701192fb8c
Giorgio Giannotta <jorge_upa@msn.com>
2021-06-15 10:30:46 +0200

    Done New UI

--- Drink Water/ContentView.swift
+++ Drink Water/ContentView.swift
@@ -11,47 +11,85 @@ struct ContentView: View {
     var screenWidth = UIScreen.main.bounds.width
     @State var dynamicHeight = 0
     @State var text = 0
+    @State var limitDrink = 2500
+    @State var percentage = 0
     var body: some View {
-        VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+        VStack(alignment: .center, spacing: nil, content: {
             ZStack{
                 Rectangle()
-                    .padding(.bottom,0)
-                    .frame(width: screenWidth, height: CGFloat(dynamicHeight), alignment: .center)
-                    .foregroundColor(.blue)
-                    .overlay(
-                        HStack(alignment: .center, spacing: nil, content: {
-                            Button(action: {
-                                if dynamicHeight > 0 {
-                                    dynamicHeight -= 100
-                                    text -= 250
-                                }
-                            }, label: {
-                                Circle()
-                                    .frame(width: 100, height: 100, alignment: .center)
-                                    .foregroundColor(.red)
-                                    .overlay(Text("Remove").foregroundColor(.white))
-                            })
-                            .padding()
-                            Spacer()
-                            Text("\(text)")
-                                .foregroundColor(.white)
-                            Spacer()
-                            Button(action: {
-                                if dynamicHeight <= 900{
-                                    dynamicHeight += 100
-                                    text += 250
-                                }
-                            }, label: {
-                                Circle()
-                                    .frame(width: 100, height: 100, alignment: .center)
-                                    .foregroundColor(.green)
-                                    .overlay(Text("Add").foregroundColor(.white))
-                            })
-                            .padding()
-                        })
-                        ,alignment: .center
-                    )
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                    .foregroundColor(.clear)
+//                    .overlay(
+//                        WaterFill(screenWidth: screenWidth, dynamicHeight: dynamicHeight, text: text)
+//                        ,alignment: .bottom
+//                    )
+//                WaveForm()
+//                CircleWaveView(percent: percentage)
+                WaterFill(percent: percentage)
+//                    .padding()
             }
+            .edgesIgnoringSafeArea(.all)
+            .overlay(
+                VStack(alignment: .center, spacing: nil, content: {
+                    HStack(alignment: .center, spacing: nil, content: {
+                        Text("Drink Water")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .shadow(color: .black, radius: 2, x: 0.3, y: 0.0)
+                        Spacer()
+                        Button(action: {
+                            //Action
+                        }, label: {
+                            Image(systemName: "gear")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .shadow(color: .black, radius: 2, x: 0, y: 0)
+                        })
+                        
+                    })
+                    .padding(.vertical, 30)
+                    .padding(.horizontal)
+                    
+                    Spacer()
+                    HStack(alignment: .center, spacing: nil, content: {
+                        Button(action: {
+//                            if dynamicHeight > 0 {
+//                                dynamicHeight -= 100
+//                                text -= 250
+//                            }
+                            if percentage > 0 {
+                                percentage -= 10
+                            }
+                        }, label: {
+                            Circle()
+                                .frame(width: 100, height: 100, alignment: .center)
+                                .foregroundColor(Color(#colorLiteral(red: 0.8881979585, green: 0.3072378635, blue: 0.2069461644, alpha: 1)))
+                                .overlay(Text("Remove").foregroundColor(.white))
+                                .shadow(color: .black, radius: 5, x: 0.8, y: 0.8)
+                        })
+                        .padding()
+                        Spacer()
+                        Button(action: {
+//                            if dynamicHeight <= 900{
+//                                dynamicHeight += 100
+//                                text += 250
+//                            }
+                            if percentage < 100 {
+                                percentage += 10
+                            }
+                        }, label: {
+                            Circle()
+                                .frame(width: 100, height: 100, alignment: .center)
+                                .foregroundColor(Color(#colorLiteral(red: 0.262835294, green: 0.8022480607, blue: 0.3886030316, alpha: 1)))
+                                .overlay(Text("Add").foregroundColor(.white))
+                                .shadow(color: .black, radius: 5, x: 0.8, y: 0.8)
+                        })
+                        .padding()
+                    })
+                })
+                ,alignment: .center
+            )
+            
         })
         
     }
---
+++ Drink Water/WaterFill.swift
@@ -0,0 +1,50 @@
+//
+//  WaterFill.swift
+//  Drink Water
+//
+//  Created by Jorge Giannotta on 14/06/21.
+//
+
+import SwiftUI
+
+struct WaterFill: View {
+    
+        @State private var waveOffset = Angle(degrees: 0)
+        let percent: Int
+        
+        var body: some View {
+
+            GeometryReader { geo in
+                ZStack {
+                    Text("\(self.percent)%")
+                        .foregroundColor(.black)
+                        .font(Font.system(size: 0.25 * min(geo.size.width, geo.size.height) ))
+                    RoundedRectangle(cornerSize: CGSize(width: 20, height: 10))
+                        .foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center)
+                        .overlay(
+                            Wave(offset: Angle(degrees: self.waveOffset.degrees), percent: Double(percent)/100)
+                                .fill(Color(red: 0, green: 0.5, blue: 0.75, opacity: 0.5))
+                                .clipShape(Rectangle().scale(1))
+                        )
+                }
+                .overlay(
+                    Text("\(self.percent)%")
+                            .foregroundColor(.black)
+                            .font(Font.system(size: 0.25 * min(geo.size.width, geo.size.height)))
+                )
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .onAppear {
+                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                self.waveOffset = Angle(degrees: 360)
+                }
+            }
+        }
+    }
+
+struct WaterFill_Previews: PreviewProvider {
+    static var previews: some View {
+        WaterFill(percent: 20)
+    }
+}
